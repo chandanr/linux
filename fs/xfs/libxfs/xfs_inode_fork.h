526ef7c44364136d6f609229efbe586faf8a0c09
Chandan Babu R <chandanrlinux@gmail.com>
2020-08-12 18:31:21 +0530

    xfs: Check for extent overflow when deleting an extent
    Deleting a file range from the middle of an existing extent can cause
    the per-inode extent count to increase by 1. This commit checks for
    extent count overflow in such cases.
    
    Signed-off-by: Chandan Babu R <chandanrlinux@gmail.com>

--- fs/xfs/libxfs/xfs_inode_fork.h
+++ fs/xfs/libxfs/xfs_inode_fork.h
@@ -39,6 +39,12 @@ struct xfs_ifork {
  * extent to a fork and there's no possibility of splitting an existing mapping.
  */
 #define XFS_IEXT_ADD_NOSPLIT_CNT	(1)
+/*
+ * Removing an extent from the middle of an existing extent can cause the extent
+ * count to increase by 1.
+ * i.e. | Old extent | Hole | Old extent |
+ */
+#define XFS_IEXT_REMOVE_CNT		(1)
 
 /*
  * Fork handling.
