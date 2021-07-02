kernelorg-mirror/linux_kernel_git_djwong_xfs-linux

xfs: get our own reference to inodes that we want to scrub
When we want to scrub a file, get our own reference to the inode
unconditionally.  This will make disposal rules simpler in the long run.

Signed-off-by: Darrick J. Wong <djwong@kernel.org>

fs/xfs/scrub/common.h
@@ -128,6 +128,7 @@ int xchk_count_rmap_ownedby_ag(struct xfs_scrub *sc, struct xfs_btree_cur *cur,
 
 int xchk_setup_ag_btree(struct xfs_scrub *sc, struct xfs_inode *ip,
 		bool force_log);
+int xchk_scan_this_inode(struct xfs_scrub *sc, struct xfs_inode *ip);
 int xchk_get_inode(struct xfs_scrub *sc, struct xfs_inode *ip_in);
 int xchk_setup_inode_contents(struct xfs_scrub *sc, struct xfs_inode *ip,
 		unsigned int resblks);

chandanr/linux

fs/xfs/scrub/common.h
@@ -118,6 +118,7 @@ int xchk_count_rmap_ownedby_ag(struct xfs_scrub *sc, struct xfs_btree_cur *cur,
 int xchk_setup_ag_btree(struct xfs_scrub *sc, bool force_log);
 int xchk_get_inode(struct xfs_scrub *sc);
 int xchk_setup_inode_contents(struct xfs_scrub *sc, unsigned int resblks);
+int xchk_install_inode(struct xfs_scrub *sc, struct xfs_inode *ip);
 void xchk_buffer_recheck(struct xfs_scrub *sc, struct xfs_buf *bp);
 
 /*