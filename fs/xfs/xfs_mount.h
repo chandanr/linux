1dc7ed3d752744563c1892d62f9f0cbb04784586
Chandan Babu R <chandan.babu@oracle.com>
2021-08-11 10:23:20 +0530

    xfs: Extend per-inode extent counter widths
    This commit adds a new 64-bit per-inode data extent counter. However the
    maximum number of extents that a data fork can hold is limited to 2^48
    extents. This feature is available only when
    XFS_SB_FEAT_INCOMPAT_EXTCOUNT_64BIT feature bit is enabled on the
    filesystem. Also, enabling this feature bit causes attr fork extent counter to
    use the 32-bit extent counter that was previously used to hold the data fork
    extent counter. This implies that the attr fork can now occupy a maximum of
    2^32 extents.
    
    This commit also exposes the newly introduced XFS_IOC_BULKSTAT_V6 ioctl
    interface to user space.
    
    Signed-off-by: Chandan Babu R <chandan.babu@oracle.com>

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -288,6 +288,7 @@ typedef struct xfs_mount {
 #define XFS_FEAT_NEEDSREPAIR	(1ULL << 25)	/* needs xfs_repair */
 #define XFS_FEAT_ATOMIC_SWAP	(1ULL << 26)	/* extent swap log items */
 #define XFS_FEAT_METADIR	(1ULL << 27)	/* metadata directory tree */
+#define XFS_FEAT_NREXT64	(1ULL << 28)	/* 64-bit inode extent counters */
 
 /* Mount features */
 #define XFS_FEAT_NOATTR2	(1ULL << 48)	/* disable attr2 creation */
@@ -370,6 +371,7 @@ __XFS_HAS_FEAT(bigtime, BIGTIME)
 __XFS_HAS_FEAT(needsrepair, NEEDSREPAIR)
 __XFS_LOG_FEAT(atomicswap, ATOMIC_SWAP)
 __XFS_HAS_FEAT(metadir, METADIR)
+__XFS_HAS_FEAT(nrext64, NREXT64)
 
 /*
  * Decide if this filesystem can use log-assisted ("atomic") extent swapping.
