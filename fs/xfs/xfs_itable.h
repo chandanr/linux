9d2697b0fd52cc34bddebab853d70eea61b6f250
Chandan Babu R <chandan.babu@oracle.com>
2022-03-09 12:58:37 +0000

    xfs: Enable bulkstat ioctl to support 64-bit per-inode extent counters
    The following changes are made to enable userspace to obtain 64-bit extent
    counters,
    1. Carve out a new 64-bit field xfs_bulkstat->bs_extents64 from
       xfs_bulkstat->bs_pad[] to hold 64-bit extent counter.
    2. Define the new flag XFS_BULK_IREQ_BULKSTAT for userspace to indicate that
       it is capable of receiving 64-bit extent counters.
    
    Suggested-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Chandan Babu R <chandan.babu@oracle.com>

--- fs/xfs/xfs_itable.h
+++ fs/xfs/xfs_itable.h
@@ -19,6 +19,8 @@ struct xfs_ibulk {
 /* Only iterate within the same AG as startino */
 #define XFS_IBULK_SAME_AG	(1 << 0)
 
+#define XFS_IBULK_NREXT64	(1 << 1)
+
 /*
  * Advance the user buffer pointer by one record of the given size.  If the
  * buffer is now full, return the appropriate error code.
